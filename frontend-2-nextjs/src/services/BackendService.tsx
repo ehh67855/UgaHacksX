75bb054316fe4b8340a569347587d83527f41e9b
williampettit <14142910+williampettit@users.noreply.github.com>
2025-02-08 12:38:30 -0500

    port jwt handling logic

---
+++ frontend-2-nextjs/src/services/BackendService.tsx
@@ -0,0 +1,43 @@
+import { jwtDecode } from "jwt-decode";
+import { CustomJwtPayload } from "@/entities/CustomJwtPayload";
+
+export const BACKEND_API_URL = "http://localhost:8080/";
+
+export const formatApiUrl = (endpoint: string): string => {
+  return `${BACKEND_API_URL}/${endpoint}`;
+};
+
+export const getAuthToken = (): string | null => {
+  return window.localStorage.getItem("auth_token");
+};
+
+export const setAuthHeader = (token: string | null) => {
+  if (token !== null) {
+    window.localStorage.setItem("auth_token", token);
+  } else {
+    window.localStorage.removeItem("auth_token");
+  }
+};
+
+export const isAuthenticated = (): boolean => {
+  return getAuthToken() !== null;
+};
+
+export const isAdmin = (token: string | null): boolean => {
+  if (token == null) {
+    return false;
+  }
+
+  const decoded = jwtDecode<CustomJwtPayload>(token);
+  return decoded.role == "ADMIN";
+};
+
+export const isUser = (token: string): boolean => {
+  const decoded = jwtDecode<CustomJwtPayload>(token);
+  return decoded.role == "USER";
+};
+
+export const getLogin = (token: string): string | undefined => {
+  const decoded = jwtDecode<CustomJwtPayload>(token);
+  return decoded.sub;
+};
