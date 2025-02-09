b73aec437530086c0553e1bcf3e6cf0c3c0f2177
williampettit <14142910+williampettit@users.noreply.github.com>
2025-02-08 21:19:57 -0500

    make tokens expire after 30d instead of 1h

--- backend/src/main/java/com/sergio/jwt/backend/config/UserAuthenticationProvider.java
+++ backend/src/main/java/com/sergio/jwt/backend/config/UserAuthenticationProvider.java
@@ -34,7 +34,7 @@ public class UserAuthenticationProvider {
 
     public String createToken(UserDto user) {
         Date now = new Date();
-        Date validity = new Date(now.getTime() + 3600000); // 1 hour
+        Date validity = new Date(now.getTime() + 3600000 * 24 * 30); // 30 days
 
         Algorithm algorithm = Algorithm.HMAC256(secretKey);
         return JWT.create()
